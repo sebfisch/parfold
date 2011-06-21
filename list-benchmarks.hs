4d617fbe34383f33de1de02547e241fc4cc7e7bc
Sebastian Fischer <fischer@nii.ac.jp>
2011-06-21 15:19:45 +0200

    pass monoid operations explicitly

--- list-benchmarks.hs
+++ list-benchmarks.hs
@@ -1,6 +1,8 @@
 import Control.Parallel ( par, pseq )
 
 import Data.Monoid      ( Monoid(..), Sum(..) )
+import Data.Foldable    ( foldMap )
+
 import Data.List        ( foldl' )
 
 import Data.Sequence    ( Seq )
@@ -12,9 +14,14 @@ import Criterion.Main   ( defaultMain, bgroup, bench, whnf, Pure )
 
 main :: IO ()
 main = defaultMain [
-         bgroup "top down" [
-           bench "ordinary lists" $ sumWith foldList [1..len],
-           bench "finger trees"   $ sumWith foldSeq  [1..len]]]
+         bgroup "sequential" [
+           bench "ordinary lists" $ whnf sum [1..len],
+           bench "finger trees"   $ whnf (getSum . foldMap Sum)
+                                         (Seq.fromList [1..len])],
+         bgroup "parallel" [
+           bgroup "top down" [
+             bench "ordinary lists" $ sumWith foldList [1..len],
+             bench "finger trees"   $ sumWith foldSeq  [1..len]]]]
   where len = 10^5
 
 type ListFold m a = (a -> m) -> [a] -> m
@@ -24,16 +31,17 @@ sumWith fold = whnf (getSum . fold Sum)
 
 -- ordinary lists
 foldList :: Monoid m => ListFold m a
-foldList = foldListWithSparks numCapabilities
+{-# SPECIALIZE foldList :: ListFold (Sum Integer) Integer #-}
+foldList = foldListWithSparks numCapabilities mappend mempty
 
 -- with upper limit for sparks
-foldListWithSparks :: Monoid m => Int -> ListFold m a
-foldListWithSparks cnt f = worker cnt
+foldListWithSparks :: Int -> (m -> m -> m) -> m -> (a -> m) -> [a] -> m
+foldListWithSparks cnt append empty f = worker cnt
   where
-    worker _ []               = mempty
+    worker _ []               = empty
     worker _ [x]              = f x
-    worker n xsys | n <= 1    = foldl' mappend mempty $ map f xsys
-                  | otherwise = x `par` y `pseq` mappend x y
+    worker n xsys | n <= 1    = foldl' append empty $ map f xsys
+                  | otherwise = x `par` y `pseq` append x y
       where
         (xs,ys) = splitAt (length xsys `div` 2) xsys
         m       = n `div` 2
@@ -42,17 +50,18 @@ foldListWithSparks cnt f = worker cnt
 
 -- finger trees
 foldSeq :: Monoid m => ListFold m a
-foldSeq f = foldSeqWithSparks numCapabilities f . Seq.fromList
+{-# SPECIALIZE foldSeq :: ListFold (Sum Integer) Integer #-}
+foldSeq f = foldSeqWithSparks numCapabilities mappend mempty f . Seq.fromList
 
-foldSeqWithSparks :: Monoid m => Int -> (a -> m) -> Seq a -> m
-foldSeqWithSparks cnt f sq = worker cnt sq
+foldSeqWithSparks :: Int -> (m -> m -> m) -> m -> (a -> m) -> Seq a -> m
+foldSeqWithSparks cnt append empty f sq = worker cnt sq
   where
     worker n s = case Seq.length s of
-                   0 -> mempty
+                   0 -> empty
                    1 -> f (Seq.index s 0)
                    k -> let l       = k `div` 2
                             (xs,ys) = Seq.splitAt l s
                             m       = n `div` 2
                             x       = worker m xs
                             y       = worker (n-m) ys
-                         in x `par` y `pseq` mappend x y
+                         in x `par` y `pseq` append x y
