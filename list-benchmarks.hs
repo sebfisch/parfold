70f99cf3ff9c40923aa8dc59d4c3cfc69ef7c25f
Sebastian Fischer <fischer@nii.ac.jp>
2011-06-21 14:51:18 +0200

    use worker wrapper style

--- list-benchmarks.hs
+++ list-benchmarks.hs
@@ -28,27 +28,31 @@ foldList = foldListWithSparks numCapabilities
 
 -- with upper limit for sparks
 foldListWithSparks :: Monoid m => Int -> ListFold m a
-foldListWithSparks _ _ []               = mempty
-foldListWithSparks _ f [x]              = f x
-foldListWithSparks n f xsys | n <= 1    = foldl' mappend mempty $ map f xsys
-                            | otherwise = x `par` y `pseq` mappend x y
-  where (xs,ys) = splitAt (length xsys `div` 2) xsys
+foldListWithSparks cnt f = worker cnt
+  where
+    worker _ []               = mempty
+    worker _ [x]              = f x
+    worker n xsys | n <= 1    = foldl' mappend mempty $ map f xsys
+                  | otherwise = x `par` y `pseq` mappend x y
+      where
+        (xs,ys) = splitAt (length xsys `div` 2) xsys
         m       = n `div` 2
-        x       = foldListWithSparks m f xs
-        y       = foldListWithSparks (n-m) f ys
+        x       = worker m xs
+        y       = worker (n-m) ys
 
 -- finger trees
 foldSeq :: Monoid m => ListFold m a
 foldSeq f = foldSeqWithSparks numCapabilities f . Seq.fromList
 
 foldSeqWithSparks :: Monoid m => Int -> (a -> m) -> Seq a -> m
-foldSeqWithSparks n f s =
-  case Seq.length s of
-    0 -> mempty
-    1 -> f (Seq.index s 0)
-    k -> let l       = k `div` 2
-             (xs,ys) = Seq.splitAt l s
-             m       = n `div` 2
-             x       = foldSeqWithSparks m f xs
-             y       = foldSeqWithSparks (n-m) f ys
-          in x `par` y `pseq` mappend x y
+foldSeqWithSparks cnt f sq = worker cnt sq
+  where
+    worker n s = case Seq.length s of
+                   0 -> mempty
+                   1 -> f (Seq.index s 0)
+                   k -> let l       = k `div` 2
+                            (xs,ys) = Seq.splitAt l s
+                            m       = n `div` 2
+                            x       = worker m xs
+                            y       = worker (n-m) ys
+                         in x `par` y `pseq` mappend x y
